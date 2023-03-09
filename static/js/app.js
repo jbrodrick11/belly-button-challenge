9b70b52df4685f714b0f2135c7a57ffaedfcc36b
jbrodrick11 <116480388+jbrodrick11@users.noreply.github.com>
2023-03-09 12:51:05 +1100

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -42,17 +42,20 @@ d3.json(url).then(function (data) {
 
     console.log(reversedData);
 
-    // Assign variables to hold axis data
-    let sample_data = reversedData.map((row) => row.sample_values)[0].slice(0, 10);
-    let otu_data = reversedData.map((row) =>  row.otu_ids)[0].toString().slice(0, 10);
-    let otu_label_data = reversedData.map((row) => row.otu_labels)[0].slice(0, 10);
+    // Assign variables to hold bar axis data
+    let barsample_data = reversedData.map((row) => row.sample_values)[0].slice(0, 10);
+    let barotu_data = reversedData.map((row) =>  row.otu_ids)[0].slice(0, 10);
+    let barotu_label_data = reversedData.map((row) => row.otu_labels)[0].slice(0, 10);
+    let yticks = barotu_data.slice(0,10).map(otuid => `OTU ${otuid}`).reverse()
     
+    const trace1data = barsample_data.sort((a,b) => a-b)
+
     // Update Bar Chart
     let trace1 = [
       {
-        x: sample_data,
-        y: otu_data,
-        hovertext: otu_label_data,
+        x: trace1data,
+        y: yticks,
+        hovertext: barotu_label_data,
         type: "bar",
         orientation: "h",
       },
@@ -61,22 +64,33 @@ d3.json(url).then(function (data) {
 
     Plotly.newPlot("bar", trace1);
 
+    // Assign variables to hold bubble axis data
+    let bubblesample_data = reversedData.map((row) => row.sample_values)[0];
+    let bubbleotu_data = reversedData.map((row) =>  row.otu_ids)[0];
+    let bubbleotu_label_data = reversedData.map((row) => row.otu_labels)[0];
+    
     // Update Bubble Chart
     let trace2 = [
       {
-        x: otu_data.slice(0, 10),
-        y: sample_data.slice(0, 10),
-        text: otu_label_data.slice(0, 10),
+        x: bubbleotu_data,
+        y: bubblesample_data,
+        text: bubbleotu_label_data,
         mode: "markers",
         marker: {
-          size: sample_data.slice(0, 10),
-          color: otu_data.slice(0, 10),
+          size: bubblesample_data,
+          color: bubbleotu_data,
           colorscale: "Earth",
         },
       },
     ];
 
-    Plotly.newPlot("bubble", trace2);
+    let layout = {
+      xaxis : {
+        title: "OTU ID"
+      }
+    }
+
+    Plotly.newPlot("bubble", trace2, layout);
   }
 
   // Update the value based on the dropdown menu
